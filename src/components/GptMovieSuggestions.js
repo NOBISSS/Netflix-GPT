a224211a987c841a2b9a18902b5b2927f4ea45ca
Parth Chauhan <parthchauhan25125@gmail.com>
2026-03-20 11:29:30 +0530

    Movie Results Displayed in Recommend component and stored in gptSlice

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import MovieList from './MovieList';
 
 const GptMovieSuggestions = () => {
+  const {movieResults,movieNames}=useSelector((store)=>store.gpt);
+  if(!movieNames) return <h1 className='text-white font-semibold'>No Movies Found Currently</h1>;
   return (
-    <div>GptMovieSuggestions</div>
+    <div className='overflow-hidden p-4 bg-black bg-opacity-60 text-white'>
+      {
+        movieResults.length > 0 && movieNames.map((title,index)=> <MovieList title={title} key={index} movies={movieResults[index]}/>)
+      }
+    </div>
   )
 }
 
