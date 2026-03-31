4867f276c1e858518d1c56c10f8c9908106c829f
Parth Chauhan <parthchauhan25125@gmail.com>
2026-03-31 17:23:46 +0530

    Add API endpoint for now playing movies and update hook to fetch data

---
+++ api/nowPlaying.js
@@ -0,0 +1,20 @@
+// api/nowPlaying.js
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch now playing movies" });
+  }
+}
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,10 +1,6 @@
 import { useEffect } from "react";
 import { addNowPlayingMovies } from "../utils/movieSlice";
-import { API_OPTIONS } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { MoviesEndPoints } from "../utils/apis";
-
-const {NOW_PLAYING_MOVIES}=MoviesEndPoints
 
 const useNowPlayingMovies = () => {
     const dispatch = useDispatch();
@@ -12,14 +8,18 @@ const useNowPlayingMovies = () => {
     const nowPlayingMovies=useSelector((store)=>store.movies.nowPlayingMovies);
 
     const getNowPlayingMovies = async () => {
-        const data = await fetch(NOW_PLAYING_MOVIES, API_OPTIONS);
+        try{
+        const data = await fetch("/api/nowPlaying");
         const json = await data.json();
         dispatch(addNowPlayingMovies(json.results));
+        }catch(error){
+            console.error("Failed to Fetch Now Playing Movies",error);
+        }
     }
 
     useEffect(() => {
         !nowPlayingMovies && getNowPlayingMovies();
-    }, [])
+    }, [nowPlayingMovies])
 }
 
 export default useNowPlayingMovies;
